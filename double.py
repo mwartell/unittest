f0b2380bf76c6eb84cfb885b95d89e0c6ae3b7f8
matt wartell <matt.wartell@gmail.com>
2016-10-04 17:19:53 -0400

    init

---
+++ double.py
@@ -0,0 +1,14 @@
+def double(n):
+    """Returns the double of n using the standard * operator.
+       Sequences and numbers are acceptable input."""
+    return n * 2
+
+def test_double_number():
+    assert double(2) == 4
+    assert double(1.5) == 3
+
+def test_double_string():
+    assert double('any') == 'anyany'
+
+def test_double_list():
+    assert double([1, 2]) == [1, 2, 1, 2]
