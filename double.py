bf2864bc7401c8683e60992233fce6f552cf4680
matt wartell <matt.wartell@gmail.com>
2016-10-04 18:31:53 -0400

    pep8 compliant

--- double.py
+++ double.py
@@ -3,12 +3,15 @@ def double(n):
        Sequences and numbers are acceptable input."""
     return n * 2
 
+
 def test_double_number():
     assert double(2) == 4
     assert double(1.5) == 3
 
+
 def test_double_string():
     assert double('any') == 'anyany'
+
 
 def test_double_list():
     assert double([1, 2]) == [1, 2, 1, 2]
